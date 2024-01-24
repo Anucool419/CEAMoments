0f1753cc494d5b5f011663ec3674d534ec31e97d
Anucool419 <131336646+Anucool419@users.noreply.github.com>
2024-01-24 22:36:17 +0530

    Add files via upload

--- Home.js
+++ Home.js
@@ -1,6 +1,12 @@
 import React from 'react'
 import BGI from "../assets/bgi.jpg" ;
-import Sogo from "../assets/image.jpg"
+import Cogo from "../assets/clogo.jpg" ;
+import Logo from "../assets/glogo.jpg" ;
+import Eogo from "../assets/elogo.jpg" ;
+import Pogo from "../assets/plogo.jpg" ;
+import Mogo from "../assets/megha.jpg" ;
+import Jogo from "../assets/john.jpg" ;
+import eogo from "../assets/Elinor.jpg" ;
 import {Link} from "react-router-dom"
 import {CSSTransition} from 'react-transition-group';
 
@@ -14,9 +20,15 @@ function Home() {
       
       <h1>CAMPUS AMBASSADOR</h1>
       <h2>The National-level Technical Symposium Of Civil Engineering,NITT</h2>
-      <p1>The Campus Ambassador program for Moments at NIT Trichy is an exciting opportunity for dynamic individuals to become the face of our National-level Technical Symposium in Civil Engineering. As a Campus Ambassador, you'll play a crucial role in spreading the word about Moments, fostering a sense of enthusiasm within your college community.<br></br>
+      <h3>Moments</h3>
+      <p1>The Campus Ambassador program for Moments at NIT Trichy is an exciting opportunity for dynamic individuals to become the face of <br></br>
+      our National-level Technical Symposium in Civil Engineering. As a Campus Ambassador, you'll play a crucial role in spreading the word<br></br>
+       about Moments, fostering a sense of enthusiasm within your college community.<br></br>
 
-Joining us as a Campus Ambassador means being a part of a vibrant team dedicated to promoting knowledge exchange, innovation, and technical prowess. Your role involves engaging with fellow students, organizing promotional events, and sharing the excitement surrounding Moments. As an ambassador, you'll be at the forefront of connecting your peers with the enriching experiences Moments has to offer, contributing to the success of our symposium.<br></br>
+Joining us as a Campus Ambassador means being a part of a vibrant team dedicated to promoting
+ knowledge exchange, innovation, and technical prowess. <br></br>Your role involves engaging with fellow students,
+  organizing promotional events, and sharing the excitement surrounding Moments. As an ambassador, you'll be at the<br></br>
+  forefront of connecting your peers with the enriching experiences Moments has to offer, contributing to the success of our symposium.<br></br>
 
 Embrace the chance to develop leadership skills, expand your network, and be a catalyst for the thriving technical culture at NIT Trichy. Join us in creating memorable Moments and inspiring the next generation of engineers!
 
@@ -25,14 +37,114 @@ Embrace the chance to develop leadership skills, expand your network, and be a c
 
 
 
-</p1>
+</p1><br></br><br></br><br></br><br></br><br></br><br></br>
+<div className="vis">
+ <h1> Vision </h1>
+  <p>Our vision is to create a network of ambassadors<br></br> who embody the spirit of our institution, 
+  championing its <br></br>values and contributing to the overall growth and success <br></br>of academic and extracurricular initiatives.
+  Through the program, we <br></br>aspire to cultivate a sense of pride and enthusiasm<br></br>  among ambassadors, motivating them to actively
+   engage with their <br></br>peers and promote a positive and inclusive campus environment.</p><br></br>
 
- <div className='image1'>
-       
-     <img src={Sogo} width={250} height={250}/>
-      
+</div>
+<div className='motive'>
+  <h1>Motive</h1>
+  <p>The Campus Ambassador Program seeks<br></br> to be a catalyst for change,<br></br> encouraging
+   ambassadors to be proactive in organizing events, sharing <br></br>information, 
+  and inspiring their fellow students to explore new horizons. <br></br>
+  Ultimately, our vision is to build a community of <br></br>
+  ambassadors who leave a lasting impact on their campuses,<br></br> shaping a legacy of
+   <br></br>collaboration, learning, and excellence.</p></div><br></br>
+   <div className="role">
+ <h1> Role </h1>
+  <p>Our vision is to create a network of ambassadors<br></br> who embody the spirit of our institution, 
+  championing its <br></br>values and contributing to the overall growth and success <br></br>of academic and extracurricular initiatives.
+  Through the program, we <br></br>aspire to cultivate a sense of pride and enthusiasm<br></br>  among ambassadors, motivating them to actively
+   engage with their <br></br>peers and promote a positive and inclusive campus environment.</p><br></br></div>
+   <div className='mission'>
+   <h1>Mission</h1>
+  <p>Our mission is to cultivate a community of dynamic and <br></br>
+  enthusiastic Campus Ambassadors who play
+   a pivotal role in fostering a<br></br> culture of leadership, collaboration, and knowledge-sharing within <br></br>
+   their university.  Our
+   Campus Ambassador Program<br></br> is committed to creating
+    an environment where ambassadors<br></br>
+   thrive as leaders, influencers, and advocates, 
+   leaving an<br></br> indelible mark  on the journey of both<br></br>
+    themselves and their peers.
+
+</p></div><br></br>
+   <br></br><br></br><br></br><br></br><br></br><br></br>
+ <div className='Responsibility'>
+ <h1>Responsibilities</h1><br></br><br></br><br></br><br></br><br></br><br></br><br></br><br></br><br></br><br></br>
+ <br></br><br></br><br></br><br></br><br></br><br></br><br></br><br></br><br></br>
+  <p> The responsibilities of a Campus Ambassador:
+ <ul><li> Promotion and Outreach</li>
+ <li>Event Organization</li>
+ <li>Communication</li>
+ <li>Networking</li>
+ <li>Feedback and Improvement</li>
+ <li>Social Media Management</li>
+ <li>Content Creation</li>
+ <li>Recruitment Assistance</li>
+ <li>Represent the Program</li>
+ <li>Adherence to Guidelines</li></ul>
+</p>
+ </div>
+ 
+     <div className='Adv'>
+      <h1>ADVANTAGES OF BECOMING CAMPUS AMBASSADOR</h1><br></br>
+      <div className='Adv1'>
+      <img src={Cogo} width={250} height={250}/>
+      <p>Get Certificates that you can add to your resume</p></div><br></br>
+      <div className='Adv2'>
+      <img src={Logo} width={250} height={250}/>
+     <p> Get the Opportunity to win Prizes!</p></div><br></br>
+      <div className='Adv3'>
+      <img src={Eogo} width={250} height={250}/>
+      <p>Receive an Appreciation letter</p></div><br></br>
+      <div className='Adv4'>
+      <img src={Pogo} width={250} height={250}/>
+     <p> Get free passes to exclusive events</p></div></div> <br></br> 
+     
+      <div className='feedback'>
+        <h1>STUDENT FEEDBACK</h1>
+        <div className='stu'>
+        <img src={Mogo} width={250} height={250}/>
+        <img src={Jogo} width={250} height={250}/>
+        <img src={eogo} width={250} height={250}/>
+        </div>
+        <p> Let us explore the experiences of some of the campus ambassadors of last year<br></br>
+          John:<br></br>
+"Being a Campus Ambassador for Moments has been an incredible experience! The program not only provided a platform for <br></br>
+me to showcase my leadership skills but also allowed me to connect with like-minded individuals. The events were engaging,<br></br> 
+the workshops were insightful, and I felt a sense of pride representing Moments at my college. It's more than just a program;<br></br> 
+it's a community that fosters growth and collaboration."<br></br><br></br>
+
+Megha:<br></br>
+"Moments exceeded my expectations! As a Campus Ambassador, I had the privilege of witnessing firsthand the impact it had <br></br>
+on students. The diverse range of activities catered to various interests, and the positive energy was contagious.<br></br>
+The organizers were supportive, and the networking opportunities were invaluable. Moments has left a lasting impression on me,<br></br>
+ and I'm excited to continue being a part of this inspiring community."<br></br><br></br>
+
+Elinor:<br></br>
+"Attending Moments as a Campus Ambassador was a transformative experience. The emphasis on innovation,<br></br> 
+coupled with the vibrant atmosphere, made every event memorable. I appreciated the program's commitment to inclusivity<br></br>
+ and the opportunities it provided to connect with industry professionals. Moments is not just an event; it's a catalyst for<br></br>
+  personal and professional growth. I'm grateful for the chance to contribute to its success."<br></br><br></br>
+
+These testimonials emphasize the positive impact of Moments on the Campus Ambassadors, highlighting the program's engaging events,<br></br>
+ supportive community, and the opportunities it provided for personal and professional development.<br></br><br></br>
+ <p1>REGISTER NOW! TO BE ONE OF THE ABOVE LUCKY AND TALENTED STUDENTS!!</p1>
+
+
+
+
+
+
+</p>
+      </div>
      </div>
-     </div>
+     
     </div>
   )
 }
